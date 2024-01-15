32f814e9aead7d11b1bb33cb6d6c3a06bf88fa1c
Daniil Melnikov <156690571+JustDaniBTW@users.noreply.github.com>
2024-01-15 19:21:39 +0300

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-"""Файл конфигурации""" """Config file"""
-
-HOST = '127.0.0.1'
-USER = 'postgres'
-PASSWORD = 'admin'
-DB_NAME = 'mostransavto'
-BOT_TOKEN = '6146915904:AAHOiDB4fMtHrSOHqPJRjjF9Llkxz_tzgc8'
-DEPARTMENTS = ['Рабочий', 'Инженер'] #specify departments here
-ADMIN_IDS = [475185446]
